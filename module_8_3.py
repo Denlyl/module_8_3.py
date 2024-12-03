5951d220f5fb7b2d4b1ea9dc9b7b2c456231231b
Denlyl <deny1skz@gmail.com>
2024-12-03 23:49:14 +0300

    Add files via upload

--- module_8_3.py
+++ module_8_3.py
@@ -3,8 +3,10 @@ class Car:
         self.model = model
         self.__vin = __vin
         self.__numbers = __numbers
-        self.__is_valid_vin(__vin)
-        self.__is_valid_numbers(__numbers)
+        if self.__is_valid_vin(__vin):
+            self.__vin = __vin
+        if self.__is_valid_numbers(__numbers):
+            self.__numbers = __numbers
 
     def __is_valid_vin(self, vin_number):
         if not isinstance(vin_number, int):
